559610b03a9351b0175d32220833793e2be1b07c
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-08-26 11:14:45 -0700

    init react faq demo app

---
+++ rct-faqdemo-appv1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+    </div>
+  );
+}
+
+export default App;
