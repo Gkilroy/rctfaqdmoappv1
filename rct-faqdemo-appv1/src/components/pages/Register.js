5a086be51fe85f42398dd592f9076162aa4b3dd1
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-08-27 12:49:34 -0700

    update register page

--- rct-faqdemo-appv1/src/components/pages/Register.js
+++ rct-faqdemo-appv1/src/components/pages/Register.js
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Register = (props) => {
+
+    const [msg, setMsg] = useState("");
+
+    const handleRegister = (e) =>{
+        setMsg("** handleRegister")
+    }
 
     return (
      <>
@@ -11,8 +18,9 @@ const Register = (props) => {
             <span>* confirm password: </span><input type="password" maxLength={10} placeholder="* confirm password required"/> <br/>
             <p></p>
             <input type="checkbox"/><span> Terms and Services</span>
+            <p>{msg}</p>
             <p></p>
-            <p></p><button>register</button>
+            <p></p><button onClick={handleRegister}>register</button>
         </div>
         <p></p>
             <Link to="/login">login</Link>
