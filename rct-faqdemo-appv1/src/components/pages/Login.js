39d5fda9064022347d9ac005b8de50165c1c227c
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-08-27 09:49:06 -0700

    update login and register page

--- rct-faqdemo-appv1/src/components/pages/Login.js
+++ rct-faqdemo-appv1/src/components/pages/Login.js
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom";
 const Login = (props) => {
 
     return (
-        <>
-            <p>Login Page</p>
-            <p></p>
-            <Link to="/register">register</Link>
-        </>
+    <>
+        <p>Login Page - v1.0.1</p>
+        <div>
+            <span>* username: </span><input type="text" maxLength={20} placeholder="* username required"/> <br/>
+            <span>* password: </span><input type="password" maxLength={10} placeholder="* password required"/> <br/>
+            <p></p><button>login</button>
+        </div>
+        <p></p>
+        <Link to="/register">register</Link>
+    </>
     )
 }
 
--- rct-faqdemo-appv1/src/components/pages/Register.js
+++ rct-faqdemo-appv1/src/components/pages/Register.js
@@ -3,11 +3,20 @@ import { Link } from "react-router-dom";
 const Register = (props) => {
 
     return (
-        <>
-            <p>Register Page</p>
+     <>
+        <p>Register Page</p>
+        <div>
+            <span>* username: </span><input type="text" maxLength={20} placeholder="* username required"/> <br/>
+            <span>* password: </span><input type="password" maxLength={10} placeholder="* password required"/> <br/>
+            <span>* confirm password: </span><input type="password" maxLength={10} placeholder="* confirm password required"/> <br/>
             <p></p>
+            <input type="checkbox"/><span> Terms and Services</span>
+            <p></p>
+            <p></p><button>register</button>
+        </div>
+        <p></p>
             <Link to="/login">login</Link>
-        </>
+     </>
     )
 }
 
